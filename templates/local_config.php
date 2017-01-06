677d09af3378cca430db442796994294af02c036
Yannik Sembritzki <yannik@sembritzki.me>
2017-01-06 10:21:24 +0100

    Finalize role

---
+++ templates/local_config.php
@@ -0,0 +1,17 @@
+<?php
+define('AFDB_USER',             '{{ avantfax_db_user }}');
+define('AFDB_PASS',             '{{ avantfax_db_password }}');
+define('AFDB_NAME',             '{{ avantfax_db_dbname }}');
+
+//
+//      AvantFAX System email address
+//
+// Emails from faxrcvd and notify are sent from this email address
+define('ADMIN_EMAIL', '{{ avantfax_admin_email }}');        // system return email address
+
+# fix faulty default /usr/local/bin/tesseract (installation without tesseract does not work!)
+define('OCR_BINARY', "/usr/bin/tesseract");
+# if we install tesseract anyway, let's just enable it
+define('ENABLE_OCR_SUPPORT', true);
+
+$WWWUSER = '{{ avantfax_php_user }}';
